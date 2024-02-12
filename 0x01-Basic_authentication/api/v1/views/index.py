9d3da55062defea625e3e1ea1cde3daaa0cfc066
Mwembe-Tsuma <49154717+Mwembe-Tsuma@users.noreply.github.com>
2024-02-12 18:42:55 +0300

    Update index.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -3,7 +3,6 @@
 """
 from flask import jsonify, abort
 from api.v1.views import app_views
-from flask.wrappers import Response
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -27,13 +26,13 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized():
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauthorized() -> None:
     """ Endpoint to raise a 401 error using abort """
     abort(401)
 
 
-@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
-def forbidden() -> Response:
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> None:
     """ Endpoint to raise a 403 error using abort """
     abort(403)
