463751fc2a48a4847da2173bcdab2fb29df65e11
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-02-13 11:16:14 +0000

    auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,17 +13,13 @@ class Auth:
         """
         Method to check if a route requires auth
         """
-        if path is None:
+        if path is None or excluded_paths is None or not excluded_paths:
             return True
 
-        if excluded_paths is None or not excluded_paths:
-            return True
+        path = path.rstrip('/') + '/'
+        excluded_paths = [p.rstrip('/') + '/' for p in excluded_paths]
 
-        for excluded_path in excluded_paths:
-            if fnmatch.fnmatch(path, excluded_path):
-                return False
-
-        return True
+        return path not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
         """
