8e0eb9fdb2920e8b73c46a27464f52af6e0cd208
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-02-13 10:25:46 +0000

    basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -23,3 +23,17 @@ class BasicAuth(Auth):
         base64_part = authorization_header.split(' ')[1]
 
         return base64_part
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        """Method to decode the Base64 Authorization header"""
+        if type(base64_authorization_header) == str:
+            try:
+                decoded_value = base64.b64decode(
+                    base64_authorization_header,
+                    validate=True,
+                )
+                return decoded_value.decode('utf-8')
+            except (binascii.Error, UnicodeDecodeError):
+                return None
