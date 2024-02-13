cbc5febff38d631d4840d4817f5b9a51888a2278
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-02-13 10:27:53 +0000

    basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,11 @@
 Basic Authentication module for the API
 """
 from api.v1.auth.auth import Auth
+from models.user import User
+import re
+import base64
+import binascii
+from typing import Tuple, TypeVar
 
 
 class BasicAuth(Auth):
