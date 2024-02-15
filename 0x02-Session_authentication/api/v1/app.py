3187dc319ffe65fd76026d9af82b052d81e43d1f
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-02-15 12:20:13 +0000

    app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -8,8 +8,7 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
-from api.v1.auth.session_auth import SessionAuth
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -22,8 +21,10 @@ auth_type = getenv('AUTH_TYPE', 'auth')
 if auth_type == 'auth':
     auth = Auth()
 elif auth_type == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 elif auth_type == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
 elif AUTH_TYPE == "session_exp_auth":
     from api.v1.auth.session_exp_auth import SessionExpAuth
