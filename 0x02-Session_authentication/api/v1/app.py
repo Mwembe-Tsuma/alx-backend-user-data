1c29720f60c72363994e0de3492ed0f21755ef0b
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-02-15 21:06:09 +0000

    app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -67,6 +67,7 @@ def authenticate_user():
             '/api/v1/status/',
             '/api/v1/unauthorized/',
             '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
         ]
         if auth.require_auth(request.path, excluded):
             cookies = auth.session_cookie(request)
