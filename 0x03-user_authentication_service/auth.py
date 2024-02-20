914b2d02bfdd4967074fcbbe10573a9a1756992d
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-02-20 11:10:34 +0000

    auth.py

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""auth module
+"""
+import bcrypt
+
+def _hash_password(password: str) -> bytes:
+    """
+    Hashes a password using bcrypt.
+
+    Args:
+    - password: The input password string
+
+    Returns:
+    - bytes: The salted hash of the input password
+    """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
