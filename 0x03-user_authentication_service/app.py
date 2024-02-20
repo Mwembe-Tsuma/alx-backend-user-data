43aca6e213de5d60157263f26fa35088775cd637
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-02-20 13:41:56 +0000

    app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -90,5 +90,20 @@ def profile() -> str:
         return jsonify({"message": "Internal Server Error"}), 500
 
 
+@app.route("/reset_password", methods=["POST"], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """ POST route to get a reset password token for the user."""
+    try:
+        email = request.form.get("email")
+
+        reset_token = AUTH.get_reset_password_token(email)
+
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except ValueError as e:
+        return jsonify({"message": str(e)}), 403
+    except Exception:
+        return jsonify({"message": "Internal Server Error"}), 500
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
